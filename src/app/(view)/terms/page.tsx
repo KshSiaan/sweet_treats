0fb40dfbb375a83b1661f29278346f3ef843805e
Ksh Siaan <kshsiaan@gmail.com>
2026-01-25 12:39:49 +0600

    quick

---
+++ src/app/(view)/terms/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import DOMPurify from "isomorphic-dompurify";
+import { getPage } from "@/lib/api/admin";
+
+export default async function Page() {
+  const data = await getPage("terms");
+
+  const cleanHTML = DOMPurify.sanitize(data?.data?.content || "");
+
+  return (
+    <main
+      className="p-6 text-center w-full"
+      dangerouslySetInnerHTML={{ __html: cleanHTML }}
+    />
+  );
+}
