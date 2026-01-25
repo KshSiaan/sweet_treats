31ba0bc070714531a7462ae8798eb8c37d024622
Shahibul Hasan <khandaker@sparktechagency.com>
2026-01-25 14:24:35 +0600

    quick

--- src/app/(view)/privacy/page.tsx
+++ src/app/(view)/privacy/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   const data = await getPage("privacy");
 
   const cleanHTML = DOMPurify.sanitize(data?.data?.content || "");
-
+//
   return (
     <main
       className="p-6 text-center w-full"
